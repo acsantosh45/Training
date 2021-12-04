f6d4e300166245be4e3bd909515f8c748891eb7e
sss <sss>
2021-12-04 16:55:32 -0600

    Added day 1

---
+++ src/day1/Welcome.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Welcome {
+    public static void main(String[] args) {
+        String greeting = "\n Welcome to Core Java fourth session 2021 is in development !";
+        System.out.println(greeting);
+        for (int i = 0; i < greeting.length(); i++) {
+            System.out.print("=");
+
+        }
+        System.out.println();
+    }
