160f01eb67f2079b8b6156598f9073b094aee012
sss <sss>
2021-12-22 20:19:10 -0600

    Enum Practise

--- src/day10/GradeCalculator.java
+++ src/day10/GradeCalculator.java
@@ -4,36 +4,35 @@ import java.util.Scanner;
 
 public class GradeCalculator {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
 
-        System.out.println("Number scored in mathematics:");
-        int mathematics = in.nextInt();
+        int[] marks = new int[5];
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Number scored in science:");
-        int science = in.nextInt();
+        for (int i = 0; i < marks.length; i++) {
+            do {
+                System.out.println("Enter a mark of subject 1!");
+                while (!sc.hasNextInt()) {
+                    System.out.println("Please enter a valid number");
+                    sc.next(); // this is important!
+                }
+                marks[i] = sc.nextInt();
+            } while (marks[i] <= 0 || marks[i] > 100);
+            System.out.println("Thank you! Got " + marks[i]);
 
-        System.out.println("Number scored in social:");
-        int social = in.nextInt();
+            int total = 0;
+            int avg = total / 5;
 
-        System.out.println("Number scored in computer:");
-        int computer = in.nextInt();
+            if (avg >= 90)
+                System.out.println("Your grade is: A");
+            else if (avg >= 80 && avg < 90)
+                System.out.println("Your grade is: B");
+            else if (avg >= 70 && avg < 80)
+                System.out.println("Your grade is: C");
+            else if (avg >= 60 && avg < 70)
+                System.out.println("Your grade is: D");
+            else
+                System.out.println("Your grade is: F");
 
-        System.out.println("Number scored in opt:");
-        int opt = in.nextInt();
-
-        int total = mathematics + science + social + computer + opt ;
-        int avg = total / 5;
-
-        if(avg>=90)
-            System.out.println("Your grade is: A");
-        else if(avg>=80 && avg<90)
-            System.out.println("Your grade is: B");
-        else if(avg>=70 && avg<80)
-            System.out.println("Your grade is: C");
-        else if(avg>=60 && avg<70)
-            System.out.println("Your grade is: D");
-        else
-            System.out.println("Your grade is: F");
-
+        }
     }
 }
