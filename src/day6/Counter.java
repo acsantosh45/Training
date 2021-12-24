1cd1a949bd721bc796bddd490b9f99c25392aca5
sss <sss>
2021-12-24 13:12:46 -0600

    day6 added

---
+++ src/day6/Counter.java
@@ -0,0 +1,22 @@
+package day6;
+
+//utility class for counting
+public class Counter {
+
+        private static int personCount;
+
+        private Counter() {
+        }
+
+        public static void increaseCount(){
+            personCount++;
+        }
+
+        public static void decreaseCount(){
+            personCount--;
+        }
+
+        public static int getCount(){
+            return personCount;
+        }
+    }
